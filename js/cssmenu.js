e691178dd222df6406db7473de42f7844d24bf9c
Ricky Palaguachi <rickyp2413@gmail.com>
2022-04-11 16:30:12 -0400

    init commit

---
+++ js/cssmenu.js
@@ -0,0 +1,25 @@
+// document.getElementById("cssmenu").innerHTML =
+// "<ul>" +
+//     "<li class='menu-title'><span><img src='https://www.njit.edu/corporate/uicomponents/images/logo.png' alt='New Jersey Institute of Technology' id='njitlogo' height='65' width='180'></span></li>" +
+//     "<li><a href='index.html'><span>Home</span></a></li>" +
+//     "<li class='has-sub'><a href='experiment-trial-videos.html'><span>Experiments</span></a><ul>" +
+//         "<li><a href='experiment-trial-videos.html'><span>Trial Videos</span></a></li>" +
+//         "<li><a href='experiment-details.html'><span>Details</span></a></li>" +
+//     "</ul></li>" +
+//     "<li class='has-sub'><a href='simulation-demo.html'><span>Simulations</span></a><ul>" +
+//         "<li><a href='simulation-demo.html'><span>Demo</span></a></li>" +
+//         "<li><a href='simulation-details.html'><span>Details</span></a></li>" +
+//     "</ul></li>" +
+//     "<li><a href='linear-stability.html'><span>Linear Stability</span></a></li>" +
+//     "<li><a href='image-analysis.html'><span>Image Analysis</span></a></li>" +
+// "</ul>";
+
+
+document.getElementById("cssmenu").innerHTML = 
+"<ul>" + 
+    "<li class='menu-title'><span><img src='https://www.njit.edu/corporate/uicomponents/images/logo.png' alt='New Jersey Institute of Technology' id='njitlogo' height='65' width='180'></span></li>" +
+    "<li><a href='index.html'><span>Home</span></a></li>" +
+    "<li><a href='simulations.html'><span>Simulations</span></a></li>" +
+    "<li><a href='results.html'><span>Interpreting Results</span></a></li>" +
+    "<li><a href='https://github.com/idivjyot/DLAFrostSimulation'><span>View Source</span></a></li>" +
+"</ul>";
